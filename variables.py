db115fcd0bb2c3cfb8e7d57bd75069c0a05d2665
Tharunraghav <100985597+Tharunraghav@users.noreply.github.com>
2024-05-09 07:06:35 +0530

    Add files via upload

---
+++ variables.py
@@ -0,0 +1,13 @@
+'''Variable'''
+
+numb=15
+print("The addition of two number is:",numb+1);
+print("The Subraction of two number is:",numb-5);
+print("The Multiplication of two number is:",numb*5);
+print("The Division of two number is:",numb/5);
+print("The Modulus of two number is:",numb%5)
+
+'''String Concatenation '''
+
+name1="ASP";
+print(name1+" "+"hero");
